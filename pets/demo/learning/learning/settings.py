0f977e9a6d5db9432e9dcc35e73cf6aa5474502d
Timurentsiya <timuryudin@icloud.com>
2021-10-18 14:22:20 +0300

    Deploy to Heroku

--- pets/demo/learning/learning/settings.py
+++ pets/demo/learning/learning/settings.py
@@ -11,13 +11,10 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-import django_heroku
-#STATIC_ROOT = BASE_DIR / 'staticfiles'
-#django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
@@ -122,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+django_heroku.settings(locals())
