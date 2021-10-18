c9e07cf557e32383879b21cb288d683b3b2bb53e
Timurentsiya <timuryudin@icloud.com>
2021-10-18 14:09:42 +0300

    Deploy to Heroku

--- pets/demo/learning/learning/settings.py
+++ pets/demo/learning/learning/settings.py
@@ -11,12 +11,11 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
 import os
-import django_heroku
-
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+import django_heroku
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 django_heroku.settings(locals())
 
